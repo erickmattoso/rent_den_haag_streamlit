e32141aa3b0313a3a426de91cd2f3535a7277283
erickmattoso <mattosoerick@gmail.com>
2021-10-12 18:24:00 +0200

    reducing size csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,7 @@ import streamlit as st
 
 # read data
 # df_pararius
-df_pararius = pd.read_csv('df_coo_pararius.csv', index_col=[0])
-
-# yelp
-#yelp = pd.read_csv('yelp.csv', index_col=[0])
+df_pararius = pd.read_csv('df_coo_pararius2.csv', index_col=[0])
 
 # create streamlit page
 st.set_page_config(layout="wide")
@@ -29,11 +26,11 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 st.title("Places to rent in The Netherlands")
 
 # create filter on sidebar
-city = st.sidebar.multiselect("city", options=list(
-    df_pararius['city'].unique()), default=['Den Haag'])
+Plaats = st.sidebar.multiselect('Plaats', options=list(
+    df_pararius['Plaats'].unique()), default=['Den Haag'])
 
 # apply filter
-df_pararius = df_pararius[df_pararius['city'].isin(city)]
+df_pararius = df_pararius[df_pararius['Plaats'].isin(Plaats)]
 
 # Filter for price
 max_price = int(df_pararius['price'].max())
@@ -46,8 +43,8 @@ price_selected_1 = col2.number_input(
 price_selected = [price_selected_0, price_selected_1]
 
 # Filter for area
-max_area = int(df_pararius['Living area'].max())
-min_area = int(df_pararius['Living area'].min())
+max_area = int(df_pararius['surface-area'].max())
+min_area = int(df_pararius['surface-area'].min())
 area_selected = st.sidebar.slider(
     'Total Area', min_area, max_area, (45, max_area))
 
@@ -58,10 +55,10 @@ with my_expander:
         df_pararius['interior'].unique()), default=['Furnished'])
     status_selected = st.multiselect("Status", options=list(
         df_pararius['status'].unique()), default=list(df_pararius['status'].unique()))
-    max_room = int(df_pararius['Rooms'].max())
-    min_room = int(df_pararius['Rooms'].min())
+    max_room = int(df_pararius['number-of-rooms'].max())
+    min_room = int(df_pararius['number-of-rooms'].min())
     room_selected = st.slider(
-        'Rooms', min_room, max_room, (min_room, max_room))
+        'number-of-rooms', min_room, max_room, (min_room, max_room))
 
 # organize filter
 filter_ = \
@@ -69,10 +66,10 @@ filter_ = \
     (df_pararius['status'].isin(status_selected)) & \
     (df_pararius['price'] >= price_selected[0]) & \
     (df_pararius['price'] <= price_selected[1]) & \
-    (df_pararius['Living area'] >= area_selected[0]) & \
-    (df_pararius['Living area'] <= area_selected[1]) & \
-    (df_pararius['Rooms'] >= room_selected[0]) & \
-    (df_pararius['Rooms'] <= room_selected[1])
+    (df_pararius['surface-area'] >= area_selected[0]) & \
+    (df_pararius['surface-area'] <= area_selected[1]) & \
+    (df_pararius['number-of-rooms'] >= room_selected[0]) & \
+    (df_pararius['number-of-rooms'] <= room_selected[1])
 
 
 # apply filter
@@ -127,9 +124,9 @@ lons = good['longitude'].tolist()
 deal = good['deal'].tolist()
 price = good['price'].tolist()
 irl = good['irl'].tolist()
-area = good['Living area'].tolist()
-rooms = good['Rooms'].tolist()
-garden = good['garden area'].tolist()
+area = good['surface-area'].tolist()
+rooms = good['number-of-rooms'].tolist()
+garden = good['garden-surface-area'].tolist()
 index = good.index.tolist()
 image = good['image'].tolist()
 
@@ -147,8 +144,8 @@ folium.LayerControl().add_to(pararius)
 st.write(pararius)
 
 # plot data on streamlit
-good_ = good[['img', 'price', 'link', 'agency', 'Living area',
-              'Rooms', 'garden area']].to_html(escape=False)
+good_ = good[['img', 'price', 'link', 'agency', 'surface-area',
+              'number-of-rooms', 'garden-surface-area']].to_html(escape=False)
 
 # prepare to download
 towrite = io.BytesIO()

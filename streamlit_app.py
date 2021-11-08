37cd86a03718f150668665c33c72d97d7c0e0a86
erickmattoso <mattosoerick@gmail.com>
2021-11-08 09:36:21 +0100

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ def page_settings():
     st.title('Costs of Living')
 
     # Lorem
-    @st.cache(persist=True)
+    @st.cache()
     def fetch_and_clean_data1():
         df = pd.read_csv('costs.csv', index_col=[0])
         return df
@@ -137,7 +137,7 @@ def page_settings():
     st.title("Places to rent in The Netherlands")
 
     # Lorem
-    @st.cache(persist=True)
+    @st.cache()
     # read data
     def fetch_and_clean_data2():
         df = pd.read_csv('df_pararius.csv', index_col=[0])
